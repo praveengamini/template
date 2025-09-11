e6a2d162e952d91a0452836ce816a04d35d24f1a
Prasad rao <gorlep131@gmail.com>
2025-09-12 03:28:00 +0530

    client pushing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,64 +1,87 @@
-import React from 'react'
-import { Route,Routes } from 'react-router-dom'
-import AuthLayout from './components/auth/layout'
-import AuthLogin from './pages/auth/login'
-import AuthRegister from './pages/auth/register'
-import AdminLayout from './components/admin-view/layout'
-import AdminOrders from './pages/admin-view/orders'
-import AdminFeatures from './pages/admin-view/features'
-import AdminDashboard from './pages/admin-view/dashboard'
-import AdminProducts from './pages/admin-view/products'
-import StudentLayout from './components/student-view/layout'
-import PageNotFound from './pages/page-not-found'
-import StudentAccount from './pages/student-view/account'
-import StudentListing from './pages/student-view/listing'
-import StudentHome from './pages/student-view/home'
-import StudentCheckout from './pages/student-view/checkout'
-import CheckAuth from './components/common/CheckAuth'
-import UnAuthPage from './pages/unauth-page/UnAuthPage'
-import { useSelector } from 'react-redux'
-import { checkAuth1 } from "./store/auth-slice";
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ScrollToTop from './utilities/ScrollToTop';
+import { Toaster } from "@/components/ui/sonner";
+import AuthLayout from './components/Auth/AuthLayout';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import LandingPage from './components/Entrance/LandingPage';
+import DashBoard from './pages/UserPages/DashBoard';
+import TaskLayout from './components/User/TaskLayout';
+import CheckAuth from './components/common/CheckAuth';
+import UserProfile from './pages/UserPages/UserProfile';
+import AddTask from './pages/UserPages/AddTask';
+import ManualTask from './pages/UserPages/ManualTask';
+import NotFound from './pages/auth/NotFound';
+import UserHome from './pages/UserPages/UserHome';
+import GoalDetail from './pages/UserPages/GoalDetail';
+import SetNewPassword from './pages/UserPages/SetNewPassword';
+import TermsOfService from './components/common/TermsOfService';
+import PrivacyPolicy from './components/common/PrivacyPolicy';
+import UserFeedBack from './pages/UserPages/UserFeedBack';
+import AdminLayout from './components/admin/AdminLayout';
+import AdminDashBoard from './pages/AdminPages/AdminDashBoard';
+import Reviews from './pages/AdminPages/Reviews';
+import AdminProfile from './pages/AdminPages/AdminProfile';
+import UsersTaskStatistics from './pages/AdminPages/UsersTaskStatistics';
+import LoadingSpinner from './utilities/LoadingSpinner';
+import { useAuthInitialize } from './utilities/hooks/useAuthInitialize';
+import AdminTestimonials from './pages/AdminPages/AdminTestimonials';
+import RecentLogins from './pages/AdminPages/RecentLogins';
+import ForgotPassword from './pages/auth/ForgotPassword';
+import ChangePassword from './pages/auth/ChangePassword';
+import CandyCrushGoalMap from './pages/UserPages/CandyCrushGoalMap';
 const App = () => {
-  const { user ,isAuthenticated} = useSelector((state)=>state.auth)
-  const dispatch = useDispatch();
+  const { isLoading: authInitLoading } = useAuthInitialize();
+  const { user, isAuthenticated, isLoading } = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    dispatch(checkAuth1());
-  }, [dispatch]);
+  if (authInitLoading || isLoading) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <div>
-      <Routes>
-        <Route path='/auth' element={
-          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-            <AuthLayout/>
-          </CheckAuth>
-        }>
-          <Route path='login' element={<AuthLogin/>} />
-          <Route path='register' element={<AuthRegister/>}/>
-        </Route>
-        <Route path='/admin' element={<CheckAuth isAuthenticated={isAuthenticated} user={user} >
-          <AdminLayout/>
-        </CheckAuth>}>
-           <Route path='orders' element={<AdminOrders/>} />
-           <Route path='dashboard' element={<AdminDashboard/>} />
-           <Route path='features' element={<AdminFeatures/>} />
-           <Route path='products' element={<AdminProducts/>} />
-        </Route>
-        <Route path='/student' element={<CheckAuth isAuthenticated={isAuthenticated} user={user}>
-          <StudentLayout/>
-        </CheckAuth>} >
-        <Route path='account' element={<StudentAccount/>} />
-        <Route path='listing' element={<StudentListing/>} />
-        <Route path='home' element={<StudentHome/>} />
-        <Route path='checkout' element={<StudentCheckout/>}/>
-        </Route>
-        <Route path='*' element={<PageNotFound/>} />
-        <Route path='/unauth-page'   element={<UnAuthPage/>}/>
-      </Routes>
+      <ScrollToTop />
+      <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path='privacy-policy' element={<PrivacyPolicy/>} />
+          <Route path='terms-service' element={<TermsOfService/>} />
+          <Route path="/auth" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path='forgot-password' element={<ForgotPassword/>}/>
+            <Route path='change-password' element={<ChangePassword/>}/>
+          </Route>
+          <Route path="/user" element={<TaskLayout />}>
+            <Route path="add-task" element={<AddTask />} />
+            <Route path="dashboard" element={<DashBoard />} />
+            <Route path='home' element={<UserHome/>} />
+            <Route path='profile' element={<UserProfile/>} />
+            <Route path='add-manual' element={<ManualTask/>} />
+            <Route path="goal/:goalId" element={<GoalDetail />} />
+            <Route path='set-new-password' element={<SetNewPassword/>} />
+            <Route path='feedback' element={<UserFeedBack/>} />
+            <Route path="goal/:goalId/map" element={<CandyCrushGoalMap />} />
+          </Route>
+            <Route path='/admin' element={<AdminLayout/>}>
+              <Route path='dashboard' element={<AdminDashBoard/>} />
+              <Route path='users' element={<UsersTaskStatistics/>} />
+              <Route path='reviews' element={<Reviews/>} />
+              <Route path='set-testimonials' element={<AdminTestimonials/>} />
+              <Route path='profile' element={<AdminProfile/>} />
+              <Route path='recent-logins' element={<RecentLogins/>}/>
+              <Route />
+              <Route path='set-new-password' element={<SetNewPassword/>}/>
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Toaster richColors position="bottom-right" />
+      </CheckAuth>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+          
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from './auth-slice'
-const store = configureStore({
-    reducer:{
-        auth: authReducer
-    }
-})
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import authReducer from './auth/index.js'; 
+import taskReducer from './task/index.js';
+import feedbackReducer from './feedback/index.js';
+import adminReducer from './admin/index.js'
+import statsReducer from './landing-page/index.js'; 
+import testimonialReducer from './testimonials.js/index.js'
+import dashboardReducer from './user-dashboard/index.js';
+import loginHistoryReducer from './recent-login/index.js';
+import otpReducer from './otp/index.js'
 
-export default store
+const rootReducer = combineReducers({
+  stats: statsReducer,
+  auth: authReducer,
+  task:taskReducer,
+  feedback: feedbackReducer,
+  admin: adminReducer,
+  testimonials:testimonialReducer,
+  dashboard:dashboardReducer,
+  loginHistory:loginHistoryReducer,
+  otp: otpReducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+export default store;
